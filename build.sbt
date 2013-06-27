dd339d6db62355f936b4d43769176165f3a6064a
mromanski <MRomanski@wp-sa.pl>
2013-06-27 13:31:00 +0200

    Move to sbt.

---
+++ build.sbt
@@ -0,0 +1,18 @@
+seq(com.github.retronym.SbtOneJar.oneJarSettings: _*)
+
+name := "redump"
+
+version := "1.0"
+
+organization := "OneWebPro"
+
+scalaVersion := "2.10.2"
+
+mainClass := Some("main.Main")
+
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.10"  % "2.1.2"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.9.0" % "1.6.1"
+
+libraryDependencies += "pl.project13.scala" %% "rainbow" % "0.1"
+
---
+++ src/main/scala/TimerActor.scala
@@ -0,0 +1,38 @@
+import akka.actor.{ActorLogging, Actor, Cancellable, ActorSystem}
+import akka.util.Timeout
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mromanski
+ * Date: 11.06.13
+ * Time: 14:09
+ */
+object TimerActor extends Actor with ActorLogging{
+
+  implicit val timeout: Timeout = 5.second
+
+  implicit val executionContext: ExecutionContext = ActorSystem("MySystem").dispatcher
+
+  val system = ActorSystem("MySystem")
+
+  var scheduler: Cancellable = _
+
+  var time : Int = 0
+
+  def receive: TimerActor.Receive = {
+    case "start" => {
+      scheduler = system.scheduler.schedule(0 milliseconds, 1.second, self, "add")
+    }
+    case "add" => {
+       time +=1
+    }
+    case "stop" => {
+      scheduler.cancel()
+      println("Elapsed time: " + time.seconds)
+      Messages.line()
+      sys.exit()
+    }
+  }
+}
