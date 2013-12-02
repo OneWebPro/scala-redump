abc1b23b68ce21bbcdf5b1cd156d15716a8265ad
loki <maciej.romanski@.pl>
2013-12-02 20:51:27 +0100

    Almost done regexp.

--- src/main/scala/dumping/Patterns.scala
+++ src/main/scala/dumping/Patterns.scala
@@ -10,24 +10,13 @@ import scala.util.matching.Regex
  */
 object Patterns extends Enumeration{
   type Patterns = Regex
-  val variablesNames = "(\\[)(.*?)(\\])".r
-  val arrayBody = "\\{(.*)\\}".r
-  ///////////////////////////////////////////////////////////////
-//  val array = "(?<=\\{)[^\\}]*(?=\\})".r
-//  val arrayVar = "\\s*((array|Array)\\s*\\(\\s*\\d*\\s*\\))\\s*".r
-//  val arrayPrint = "\\s*(Array)\\s*".r
-//  val string = "\\s*(string\\s*\\(\\s*\\d*\\s*\\))\\s*".r
-//  val int = "\\s*((integer|int)\\s*\\(\\s*-?\\w*\\s*\\))\\s*".r
-//  val float = "\\s*(float\\s*\\(\\s*\\w*?.\\w*\\s*\\))\\s*".r
-//  val boolean = "\\s*((boolean|bool)\\s*\\(\\s*(true|false)*\\s*\\))\\s*".r
-//  val bracket = "(?<=\\()[^\\)]*(?=\\))".r
-//  val text = "(\\s*=>\\s*(?!\\s*array)(?!\\s*(integer|int)\\s*\\(\\s*-?\\d*\\s*\\))(?!\\s*(boolean|bool)\\s*\\(\\s*(true|false)*\\s*\\))(?!\\s*float\\s*\\(\\s*\\d*?.\\d*\\s*\\))(.*?)\\s*(\\[(\\'\\w*\\'|\\d*)\\]))".r
-//  val textBracket = "(\\s*\\'(.*?)\\s*(})\\s*?(})*\\',\\n\\t)".r
-//  val textBracket2 = "(\\s*\\'(.*?)\\s*(\\))\\s*?(\\))*\\',\\n\\t)".r
-//  val checkIsDateTime = "('(\\w*-\\w*-\\w*)\\s*\\(\\w*:\\w*\\)\\s*',\\s*)".r
-//  val textLast = "(\\s*\\'\\w*\\'=>\\s*(?!\\s*array)(?!\\s*\\(int\\)\\s*-?\\d*\\s*,)(?!\\s*\\(float\\)\\s*\\d*?.\\d*\\s*)(?!\\s*\\')(.*?)(?!\\s*\\')\\s*(\\),))".r
-//  val textLastPrint = "(\\s*\\'\\w*\\'\\s*=>\\s*(?!\\s*array)(?!\\s*\\(int\\)\\s*-?\\d*\\s*,)(?!\\s*\\(float\\)\\s*\\d*?.\\d*\\s*)(?!\\s*\\')(.*?)(?!\\s*\\')\\s*(\\)))".r
-//  val variable_name = "(?<=\\[)[^]]+(?=\\])".r
-//  val test = "(.*?)^(?!.*}).*$".r
+  val variablesNames = "(?<=\\[).*?(?=\\])".r
+  val arrayBody = "(?<=\\{).*(?=\\})".r
+  val variableInArray = ".(?<=\\[)(.*?)(?=\\]).(?<=\\s*\\])(.*?)(?=\\[)".r
+  val arrayVariable = "\\[(.*?)\\]\\s*=>\\s*array\\(\\d*\\)\\s*\\{(?<=\\{)(.*?)(?=\\})".r
+  val int = ".(?<=\\[)(.*?)(?=\\]).\\s*=>\\s*(int|integer)\\s*\\((\\d*)\\)".r
+  val string = ".(?<=\\[)(.*?)(?=\\]).\\s*=>\\s*string\\s*\\(\\d\\)\\s*(\".*?\"|\'.*?\')".r
+  val float = ".(?<=\\[)(.*?)(?=\\]).\\s*=>\\s*(float)\\s*\\((\\d*(.|,)?\\d*?)\\)".r
+  val boolean = ".(?<=\\[)(.*?)(?=\\]).\\s*=>\\s*(boolean|bool)\\s*\\((true|false)\\)".r
 }
 
